8e05841be723cd6797254576068ccdbbade40343
jinmz21 <jinminzhou0000@gmail.com>
2024-11-22 10:16:10 +0800

    [SideMenu] 跳转页面

--- flutter/lib/homepage/component/sidemenu.dart
+++ flutter/lib/homepage/component/sidemenu.dart
@@ -4,9 +4,22 @@ import 'package:pose_app/config/size_config.dart';
 import 'package:pose_app/style/colors.dart';
 
 class SideMenu extends StatelessWidget {
+  final VoidCallback onNavigateToHome;
+  final VoidCallback onNavigateToStatistics;
+  final VoidCallback onNavigateToCalendar;
+  final VoidCallback onNavigateToCommunication;
+  final VoidCallback onNavigateToSettings;
+  final VoidCallback onNavigateToSignOut;
+
   const SideMenu({
-    super.key,
-  });
+    Key? key,
+    required this.onNavigateToHome,
+    required this.onNavigateToStatistics,
+    required this.onNavigateToCalendar,
+    required this.onNavigateToCommunication,
+    required this.onNavigateToSettings,
+    required this.onNavigateToSignOut,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -18,62 +31,59 @@ class SideMenu extends StatelessWidget {
         color: AppColors.white,
         child: SingleChildScrollView(
           child: Column(
-            //mainAxisSize: MainAxisSize.max,
             children: [
-            Container(
-              height: 100,
-              alignment: Alignment.topCenter,
-              padding: EdgeInsets.only(top: 20),
-              child: SizedBox(
-                width: 35,
-                height: 35,
-                child: SvgPicture.asset('assets/icons/User_example.svg'),
+              Container(
+                height: 100,
+                alignment: Alignment.topCenter,
+                padding: EdgeInsets.only(top: 20),
+                child: SizedBox(
+                  width: 35,
+                  height: 35,
+                  child: SvgPicture.asset('assets/icons/User_example.svg'),
                 ),
-            ),
-            //主页按钮
-            IconButton(
-              onPressed:(){} , 
-              icon: SvgPicture.asset('assets/icons/Home.svg', color: AppColors.iconGray,),
-              iconSize: 20,
-              padding: EdgeInsets.symmetric(vertical: 20.0),
               ),
-            //日历按钮
-            IconButton(
-              onPressed:(){} , 
-              icon: SvgPicture.asset('assets/icons/calendar.svg', color: AppColors.iconGray,),
-              iconSize: 20,
-              padding: EdgeInsets.symmetric(vertical: 20.0),
+              //主页按钮
+              IconButton(
+                onPressed: onNavigateToHome,
+                icon: SvgPicture.asset('assets/icons/Home.svg', color: AppColors.iconGray),
+                iconSize: 20,
+                padding: EdgeInsets.symmetric(vertical: 20.0),
               ),
-            //朋友圈（广场）按钮
-            IconButton(
-              onPressed:(){} , 
-              icon: SvgPicture.asset('assets/icons/Chat.svg', color: AppColors.iconGray,),
-              iconSize: 20,
-              padding: EdgeInsets.symmetric(vertical: 20.0),
+              //日历按钮
+              IconButton(
+                onPressed: onNavigateToCalendar,
+                icon: SvgPicture.asset('assets/icons/calendar.svg', color: AppColors.iconGray),
+                iconSize: 20,
+                padding: EdgeInsets.symmetric(vertical: 20.0),
               ),
-            //统计按钮
-            IconButton(
-              onPressed:(){} , 
-              icon: SvgPicture.asset('assets/icons/pie-chart.svg', color: AppColors.iconGray,),
-              iconSize: 20,
-              padding: EdgeInsets.symmetric(vertical: 20.0),
-              ), 
-            //设置按钮
-            IconButton(
-              onPressed:(){} , 
-              icon: SvgPicture.asset('assets/icons/Setting.svg', color: AppColors.iconGray,),
-              iconSize: 20,
-              padding: EdgeInsets.symmetric(vertical: 20.0),
+              //朋友圈（广场）按钮
+              IconButton(
+                onPressed: onNavigateToCommunication,
+                icon: SvgPicture.asset('assets/icons/Chat.svg', color: AppColors.iconGray),
+                iconSize: 20,
+                padding: EdgeInsets.symmetric(vertical: 20.0),
               ),
-      
-            //退出按钮
-            IconButton(
-              onPressed:(){} , 
-              icon: SvgPicture.asset('assets/icons/signout.svg', color: AppColors.iconGray,),
-              iconSize: 20,
-              padding: EdgeInsets.symmetric(vertical: 20.0),
+              //统计按钮
+              IconButton(
+                onPressed: onNavigateToStatistics,
+                icon: SvgPicture.asset('assets/icons/pie-chart.svg', color: AppColors.iconGray),
+                iconSize: 20,
+                padding: EdgeInsets.symmetric(vertical: 20.0),
               ),
-      
+              //设置按钮
+              IconButton(
+                onPressed: onNavigateToSettings,
+                icon: SvgPicture.asset('assets/icons/Setting.svg', color: AppColors.iconGray),
+                iconSize: 20,
+                padding: EdgeInsets.symmetric(vertical: 20.0),
+              ),
+              //退出按钮
+              IconButton(
+                onPressed: onNavigateToSignOut,
+                icon: SvgPicture.asset('assets/icons/signout.svg', color: AppColors.iconGray),
+                iconSize: 20,
+                padding: EdgeInsets.symmetric(vertical: 20.0),
+              ),
             ],
           ),
         ),
