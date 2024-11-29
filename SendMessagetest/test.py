5856c39a28719d608e3ae7315f03fd6791a96cb8
Leon <118652405+LeON-Nie-code@users.noreply.github.com>
2024-11-29 13:26:14 +0800

    fix:Remove sensitive information

--- SendMessagetest/test.py
+++ SendMessagetest/test.py
@@ -2,7 +2,7 @@
 from SendCode.send import SendCode
 
 def test_SendCode():
-    result = SendCode('1234', '18610776716')
+    result = SendCode('1234', '18838223743')
     print(result)
 if __name__ == '__main__':
     test_SendCode()
